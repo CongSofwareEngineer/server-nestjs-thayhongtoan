f42923e57433a271db74978d68e2a37bc338ef86
Cong_Bacoor <cong@bacoor.co>
2025-06-20 15:23:06 +0700

    Add MongoDB configuration file with database name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DB_NAME } from './common/mongoDB';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.test.local',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.USER_NAME_MONGO}:${process.env.PASSWORD_MONGO}@tc-store-admin.mpkyxqj.mongodb.net/?retryWrites=true&w=majority&appName=tc-store-admin`,
+      {
+        dbName: DB_NAME,
+        enableUtf8Validation: true,
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
